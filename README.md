04e23744e0652f8ef47bd2de005b601e7bd4bd52
Ash <smashraful1995@gmail.com>
2022-04-15 22:14:09 +0600

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+-----project description-----
+The menu button contains the sidebar content. by default it is false.
+
+\*\*\*reuse same function globally by using 'useContext' hook.
+
+------------useContext----------------
+.useContext accepts a context object and returns the current context value for that context.
+.A component calling useContext will always re-render when the context value changes.
---
+++ src/Home.js
@@ -0,0 +1,20 @@
+import React, { useContext } from "react";
+import { FaBars } from "react-icons/fa";
+import { AppContext, useGlobalContext } from "./context";
+
+const Home = () => {
+  const { openSidebar, openModal } = useGlobalContext();
+
+  return (
+    <main>
+      <button className="sidebar-toggle" onClick={openSidebar}>
+        <FaBars />
+      </button>
+      <button className="btn" onClick={openModal}>
+        show modal
+      </button>
+    </main>
+  );
+};
+
+export default Home;
